69299556595567bb6143c1103ea19b06a84e8dd2
Beto-Master <herber.fausto@gmail.com>
2024-07-20 16:55:31 -0600

    primer commit

---
+++ src/vista/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author Fausto
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo)");
+        frm_estudiante frm = new frm_estudiante();
+        frm.show();
+        // TODO code application logic here
+        
+    }
+    
+}
